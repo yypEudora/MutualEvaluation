1e1518aacb31eb7b133cded4209a22cfad35ff50
Eudora <791188918@qq.com>
2021-02-02 13:17:22 +0800

    服务端0.1.0
    实现了服务端的注册和登录服务--uploader:yypEudora

---
+++ Mutual_Evaluation-Server/dealLogin.h
@@ -0,0 +1,29 @@
+#ifndef DEALLOGIN_H
+#define DEALLOGIN_H
+
+#include <QTcpSocket>
+#include <QSqlDatabase>
+#include "mysql.h"
+
+class DealLogin
+{
+public:
+    DealLogin(QTcpSocket *tcpSocket, MYSQL mysql);
+    ~DealLogin();
+
+    void read_login_messages(QByteArray send_buf);
+    void send_login_back_messages(QByteArray postData);
+    QByteArray set_login_back_json(QString msg);
+
+public slots:
+
+
+
+private:
+    MYSQL m_mysql; //数据库
+    QTcpSocket *m_tcpSocket;//连接进服务器的套接字
+    void get_login_info(QByteArray log_buf,QString &user, QString &pwd);
+
+};
+
+#endif // DEALLOGIN_H
